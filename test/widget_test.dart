bd72a21910bc0fb8b6051cf47a501261a4b2e86c
Wakidin01 <sanjibucin40@gmail.com>
2022-04-05 00:37:30 +0700

    commit conditional

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -77,4 +77,20 @@ void main() {
   print(a < b);
   print(a >= b);
   print(a <= b);
+
+  //Conditional
+  print('Conditional');
+  var nilai;
+  nilai = 80;
+
+  if (nilai >= 80) {
+    print('A');
+  } else if (nilai <= 80 && nilai >= 50) {
+    print('B');
+  } else {
+    print('Tidak Lulus!');
+  }
+
+  print('-------------');
+  nilai >= 80 ? print('A') : print('Tidak A');
 }
